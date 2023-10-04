10bed44563ba1a88ba385d770ca454bcf166f6f9
redthorx <55249192+redthorx@users.noreply.github.com>
2023-10-04 14:59:02 +0000

    fixed logout not removing access_token_expires

--- api_gateway_stack/authentication_stack/logout/lambda_function.py
+++ api_gateway_stack/authentication_stack/logout/lambda_function.py
@@ -5,7 +5,7 @@ def cognito_location():
 
 def lambda_handler(event, context):
         return {
-           "cookies" : ['access_token_cookie=deleted; Path=/; Max-Age=0;','csrf=deleted; Path=/; Max-Age=0;','logged_in=deleted; Path=/; Max-Age=0;'],
+           "cookies" : ['access_token_cookie=deleted; Path=/; Max-Age=0;','csrf=deleted; Path=/; Max-Age=0;','logged_in=deleted; Path=/; Max-Age=0;', 'access_token_expires=deleted; Path=/; Max-Age=0;'],
           "isBase64Encoded": False,
           "statusCode": 302,
           "headers": {
